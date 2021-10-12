bc57c44b9b16cbc0e05c42629170c017df0306d7
Chubak <musaev4t@gmail.com>
2021-10-12 16:06:34 +0300

    header styles

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from "react";
+import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/MainNav/MainNav";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <div className="app">sdfdsf</div>
+      <SimpleBottomNavigation/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from "./header.module.css"
+const Header = () => {
+  return (
+    <div>
+      <span className={style.header}>🎬 Entertainment hub 🎥</span>
+    </div>
+  );
+};
+
+export default Header;
